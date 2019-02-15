66a2ead6fbb1bcdad7426cac10f2dac2ebb9f831
officialHenrik <36031881+officialHenrik@users.noreply.github.com>
2019-02-15 13:28:21 +0100

    Update logger.py

--- logger.py
+++ logger.py
@@ -13,23 +13,14 @@ DBNAME = 'test'
 HOST = 'localhost'
 PORT = 8086
 
-ps = PySmhi()
-
 # ------------------------------------------------------
-# Callback for writing data to database
-def log_to_db():
-    
-    # get data
+def get_point(where, lat, lng):
     ps = PySmhi()
-    #w = ps.getWeather(1, 55.348446, 13.360708) # Get current weather in Smygehamn
-    w = ps.getWeather(1, 57.650989, 11.965847) # Get current weather in Eklanda
-    
-    # Insert into db
-    points = []
+    w = ps.getWeather(1, lat, lng) # Get current weather
     point = {
         "measurement": 'Temp',
         "tags": {
-            "location": "home",
+            "location": where,
             "sensor": "smhi", 
         },
         "fields": {
@@ -39,20 +30,31 @@ def log_to_db():
              "humi": w[0][3]
                 }
             }
-    points.append(point)
-    
+    return(point)
+
+# ------------------------------------------------------
+# Callback for writing data to database
+def log_to_db():
+    # Insert into db
+    points = []
+    p1 = get_point("Smygehamn", 55.348446, 13.360708) # Get current weather
+    points.append(p1)
+    p2 = get_point("Eklanda", 57.650989, 11.965847) # Get current weather
+    points.append(p2)
+    print(points)
     client = InfluxDBClient(HOST, PORT, USER, PASSWORD, DBNAME)
 
     if(client.write_points(points)):
         points = []
     else:
-       	# failure, try again next time
+       	# failure
         print("Warning: failed inserting into influxdb")
 
+        
+# ------------------------------------------------------
 # Schedule logging
 schedule.every(1).hour.do(log_to_db)
 
-# ------------------------------------------------------
 # Run forever
 try:
     schedule.run_all()
