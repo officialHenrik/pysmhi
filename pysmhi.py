70fd19632ba0656d4ad28ede520d25fadcdb7130
hgaros <hg@example.com>
2019-02-15 09:03:09 +0100

    cleaning, removed json import

--- pysmhi.py
+++ pysmhi.py
@@ -1,10 +1,8 @@
 #!/usr/bin/env python3
 
 import requests
-import json
 
 class PySmhi:
-
     # Get weather forecast
     def getWeatherForecast(self, lat, lng):
 
@@ -14,37 +12,34 @@ class PySmhi:
         r = requests.get(url)
 
         if(r.status_code == 200): #OK
-            o = json.loads(r.text)
-            # iterate the forecast
-            for w in o['timeSeries']:
-                if not w or not "parameters" in w:
-                    print("Error in response : %s" % (w))
+            jw = r.json()
+            # iterate the forecast hour by hour
+            for fc_hour in jw['timeSeries']:
+                if not fc_hour or not "parameters" in fc_hour:
+                    print("Error in response : %s" % (fc_hour))
                 else:
-                    temp = ws = wgs = rhum = 126
-                    for p in w["parameters"]:
+                    temp = ws = wgs = rhum = 126 # default fail
+                    for p in fc_hour["parameters"]:
                         param = p["name"]
                         val   = p["values"][0]
-                        if param == "t": # Temperature
-                            temp = val
-                        if param == "ws": # Wind speed
-                            ws = val
-                        if param == "gust": # Wind gust speed
-                            wgs = val
-                        if param == "r": # Relative humidity
-                            rhum = val
+                        if param == "t":      temp = val # temperature
+                        elif param == "ws":   ws = val   # Wind speed
+                        elif param == "gust": wgs = val  # Wind gust speed
+                        elif param == "r":    rhum = val # Relative humidity
                     output.append([temp, ws, wgs, rhum])
         else:
             print("Url request failed: %s" % (r.status_code))
         return(output)
 
     # Get weather
-    def getWeather(self, nof, lat, lng):
+    def getWeather(self, nof_hours, lat, lng):
         wf = self.getWeatherForecast(lat, lng)
-        return(wf[0:nof])
+        return(wf[0:nof_hours])
 
+# --------------------------------------------------------
 if __name__ == "__main__":
     ps = PySmhi()
     w = ps.getWeather(1, 55.348446, 13.360708) # Get current weather in Smygehamn
     if w:
-        for fc in w:
+        for f in w:
             print("temp: {}C, wind: {}({})m/s, relHumid: {}%".format(fc[0],fc[1],fc[2],fc[3]))
